58b993eacf5106ff59f1b1f61ce7dc788c75772b
Vishal Row Mysore <visrow@gmail.com>
2024-04-29 18:54:05 -0400

    workflow fix

--- src/main/java/com/t4a/predict/PredictionLoader.java
+++ src/main/java/com/t4a/predict/PredictionLoader.java
@@ -359,6 +359,7 @@ public class PredictionLoader {
                 }
             }
         }  catch (Exception e) {
+            e.printStackTrace();
             log.error(" Please make sure actions are configured {} ",e.getMessage());
             return null;
         }
