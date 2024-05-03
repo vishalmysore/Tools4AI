fe4ca10a77c34f48593007618c8cd0f8cddc6234
Vishal Row Mysore <visrow@gmail.com>
2024-05-03 17:38:13 -0400

    refactoring

--- src/test/java/com/t4a/test/MimeTypeTest.java
+++ src/test/java/com/t4a/test/MimeTypeTest.java
@@ -1,4 +1,19 @@
 package com.t4a.test;
 
+import com.t4a.api.MimeType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MimeTypeTest {
+    @Test
+    void testGetMimeType() {
+        Assertions.assertEquals("image/jpeg", MimeType.JPEG.getMimeType());
+        Assertions.assertEquals("image/png", MimeType.PNG.getMimeType());
+        Assertions.assertEquals("image/gif", MimeType.GIF.getMimeType());
+        Assertions.assertEquals("text/html", MimeType.HTML.getMimeType());
+        Assertions.assertEquals("text/plain", MimeType.TEXT.getMimeType());
+        Assertions.assertEquals("application/pdf", MimeType.PDF.getMimeType());
+        Assertions.assertEquals("application/msword", MimeType.MS_WORD.getMimeType());
+        Assertions.assertEquals("application/vnd.oasis.opendocument.text", MimeType.OPEN_DOCUMENT_TEXT.getMimeType());
+    }
 }
