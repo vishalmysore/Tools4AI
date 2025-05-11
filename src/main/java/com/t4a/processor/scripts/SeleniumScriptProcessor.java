e85f05f19b00017022499258e7fc2f95835c996f
Vishal Row Mysore <visrow@gmail.com>
2025-05-11 09:51:38 -0400

    Bump version to 1.0.8 and enhance script processing in SeleniumScriptProcessor

--- src/main/java/com/t4a/processor/scripts/SeleniumScriptProcessor.java
+++ src/main/java/com/t4a/processor/scripts/SeleniumScriptProcessor.java
@@ -1,6 +1,8 @@
 package com.t4a.processor.scripts;
 
 import com.google.gson.Gson;
+import com.t4a.detect.ActionCallback;
+import com.t4a.detect.ActionState;
 import com.t4a.processor.AIProcessingException;
 import com.t4a.processor.selenium.SeleniumGeminiProcessor;
 import com.t4a.processor.selenium.SeleniumProcessor;
@@ -58,6 +60,20 @@ public class SeleniumScriptProcessor {
         return result;
     }
 
+    public ScriptResult process(String content, ActionCallback callback) {
+        ScriptResult result = new ScriptResult();
+        try {
+            BufferedReader reader = new BufferedReader(new StringReader(content));
+            log.debug("Processing content string");
+            processCommands(reader, result, callback);
+        } catch (IOException e) {
+            log.error("Error processing content: " + e.getMessage());
+        } catch (AIProcessingException e) {
+            log.error("AI Processing error: " + e.getMessage());
+        }
+        return result;
+    }
+
     public void processCommands( BufferedReader reader, ScriptResult result, SeleniumCallback callback) throws IOException, AIProcessingException {
         SeleniumProcessor processor = getActionProcessor();
         String line;
@@ -71,6 +87,17 @@ public class SeleniumScriptProcessor {
         }
     }
 
+    public void processCommands( BufferedReader reader, ScriptResult result, ActionCallback callback) throws IOException, AIProcessingException {
+        SeleniumProcessor processor = getActionProcessor();
+        String line;
 
+
+        while ((line = reader.readLine()) != null) {
+            callback.sendtStatus("processing "+line, ActionState.WORKING);
+            processor.processWebAction(line);
+            callback.sendtStatus("processed "+line, ActionState.WORKING);
+            log.debug("{}",result);
+        }
+    }
 
 }
